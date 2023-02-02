c5c6b94990913e7f338b9def92a2e396970d5d68
Christian Dimitri <cdimitri@e-verse.com>
2023-02-02 12:44:10 +0100

    Add project files.

---
+++ Rhino.Algorithms.Toolkit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhino.Algorithms.Toolkit
+{
+    public class Class1
+    {
+    }
+}
